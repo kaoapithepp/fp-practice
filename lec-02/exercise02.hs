9d560dbaec3cfce5ecf9ecf4f7089138397957f8
kaoapithepp <momogg334@gmail.com>
2021-11-14 17:01:36 +0700

    done zipper

---
+++ lec-02/exercise02.hs
@@ -0,0 +1,14 @@
+-- zipper :: ([a], [b]) -> [(a, b)]
+zipper([],[]) = []
+zipper([],ys) = []
+zipper(x,[]) = []
+zipper(x:xs, y:ys) = (x,y) : zipper(xs,ys)
+
+-- test cases
+-- zipper ([1,2,3], ['a','b','c'])      ; type : Num a => [(a, Char)]
+-- zipper ([4,3,2,1], "hello")          ; type : Num a => [(a, Char)]
+-- zipper ([], "functional")            ; type : [(a, Char)]
+-- zipper ([1,2,3], [])                 ; type : Num a => [(a, b)]
+-- zipper ([1,1.5,2], [2.5,3,3.5,3])    ; type : (Fractional a, Fractional b) => [(a, b)]
+
+
