46b1eb6654eacc5dcf65145b76ede931e6d5d573
kaoapithepp <momogg334@gmail.com>
2021-11-22 22:35:41 +0700

    done exercise04

--- lec-04/fib.hs
+++ lec-04/fib.hs
@@ -1,3 +1,15 @@
+-- fib 0 = 0
+-- fib 1 = 1
+-- fib n = fib (n-1) + fib (n-2)
+
+fib :: Integral p => p -> p
+fib n
+    | n == 0 = 0
+    | n == 1 = 1
+    | n < 0 = error "negative number"
+    | n > 0 = fib (n-1) + fib (n-2)
+    | otherwise = error "not an integer"
+
 -- (2)
 -- ต้องให้มันปลอดภัยด้วยนะ เหมือนที่เรียนใน lec-04 ไปเนี่ยแหละ
 -- type คืออะไร
