5a6ef70e581eaed8b15002a9c273e761be98fb6f
kaoapithepp <momogg334@gmail.com>
2021-11-26 10:53:34 +0700

    done exercise05

---
+++ lec-05/listmap.hs
@@ -0,0 +1,14 @@
+list_map :: (a1 -> a2) -> [a1] -> [a2]
+list_map _ [] = []
+list_map (op) (x) = map op x
+
+-- test cases
+-- list_map (+1) [1,2,3,4,5,6]
+-- list_map (> 'i') "hello"
+-- list_map (\x -> x) []
+-- list_map length ["functional","gg","easy"]
+
+-- three more test cases
+-- list_map (3*) [2,4,6,8]
+-- list_map (\x -> x) ["a","b","c"]
+-- list_map (/4) [12,24,28]
---
+++ lec-05/zipper-tail.hs
@@ -0,0 +1,12 @@
+zipper l1 l2 = zipper_t l1 l2 []
+
+zipper_t :: [a] -> [b] -> [(a,b)] -> [(a,b)]
+zipper_t [] _ res = res
+zipper_t _ [] res = res
+zipper_t (x:xs) (y:ys) res = zipper_t xs ys (res ++ [(x,y)])
+
+-- test cases
+-- zipper [1,2,3] ['a','b','c']
+-- zipper [4,3,2,1] "hello"
+-- zipper [] "functional"
+-- zipper [1,2,3] []
